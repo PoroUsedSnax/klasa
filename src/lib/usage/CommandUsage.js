5c811f36d18ff78ed354d1e3f20dd0c8e6ef19f9
Mark Fisher <markf0298@gmail.com>
2020-10-18 01:29:00 +0100

    Update CommandUsage.js

--- src/lib/usage/CommandUsage.js
+++ src/lib/usage/CommandUsage.js
@@ -14,29 +14,12 @@ class CommandUsage extends Usage {
 	 * @param {usageDelim} usageDelim The usage deliminator for this command
 	 * @param {Command} command The command this parsed usage is for
 	 */
-	constructor(client, usageString, usageDelim, command) {
+	constructor(client, usageString, friendlyUsageString, usageDelim, command) {
 		super(client, usageString, usageDelim);
 
-		/**
-		 * All names and aliases for the command
-		 * @since 0.0.1
-		 * @type {string[]}
-		 */
-		this.names = [command.name, ...command.aliases];
+		this.friendlyUsage = friendlyUsageString ? ` ${friendlyUsageString}` : null;
 
-		/**
-		 * The compiled string for all names/aliases in a usage string
-		 * @since 0.0.1
-		 * @type {string}
-		 */
-		this.commands = this.names.length === 1 ? this.names[0] : `《${this.names.join('|')}》`;
-
-		/**
-		 * The concatenated string of this.commands and this.deliminatedUsage
-		 * @since 0.0.1
-		 * @type {string}
-		 */
-		this.nearlyFullUsage = `${this.commands}${this.deliminatedUsage}`;
+		this.fullUsage = `${command.parent ? `${command.parent.name} ` : ''}${command.name}${this.friendlyUsage ? this.friendlyUsage : this.deliminatedUsage}`;
 	}
 
 	/**
@@ -51,25 +34,12 @@ class CommandUsage extends Usage {
 	}
 
 	/**
-	 * Creates a full usage string including prefix and commands/aliases for documentation/help purposes
-	 * @since 0.0.1
-	 * @param {KlasaMessage} message The message context for which to generate usage for
-	 * @returns {string}
-	 */
-	fullUsage(message) {
-		let prefix = message.prefixLength ? message.content.slice(0, message.prefixLength) : message.guildSettings.get('prefix');
-		if (message.prefix === this.client.mentionPrefix) prefix = `@${this.client.user.tag}`;
-		else if (Array.isArray(prefix)) [prefix] = prefix;
-		return `${prefix.length !== 1 ? `${prefix} ` : prefix}${this.nearlyFullUsage}`;
-	}
-
-	/**
 	 * Defines to string behavior of this class.
 	 * @since 0.5.0
 	 * @returns {string}
 	 */
 	toString() {
-		return this.nearlyFullUsage;
+		return this.fullUsage;
 	}
 
 }
